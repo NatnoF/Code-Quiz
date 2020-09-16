2a0ae65f979298f1657ee7df8751bd2faffffc9f
Nicolas Fontan <nico.fontan1@gmail.com>
2020-09-16 00:00:04 -0400

    Added 5 questions for the quiz

--- questions.js
+++ questions.js
@@ -1,12 +1,27 @@
 var questions = [
     {
-      title: "Example Question 1:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "Choice 1"
+      title: "String values must be enclosed within _____ when being assigned to variables.",
+      choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+      answer: "3. quotes"
     },
     {
-      title: "Example Question 2:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "Choice 2"
+      title: "Commonly used data types DO NOT include:",
+      choices: ["1. alerts", "2. booleans", "3. strings", "4. numbers"],
+      answer: "1. alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["1. quotes", "2. parenthesis", "3. square brackets", "4. curly brackets"],
+      answer: "2. parenthesis"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store _____.",
+      choices: ["1. numbers and strings", "2. booleans", "3. other arrays", "4. all of the above"],
+      answer: "4. all of the above"
+    },
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+      answer: "4. console.log"
     }
   ];
